dd67cf342209940670fba9fd2de38bcd421b808d
John Zwarthoed <john.zwarthoed@gmail.com>
2014-02-27 15:11:14 +0100

    Add user micropost

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,7 @@ require 'spec_helper'
 describe User do
 
 	before do
-		@user = User.new(name: "Example User", email: "user@example.com",
-			password: "foobar", password_confirmation: "foobar")
+		@user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
 	end
 
 	subject { @user }
@@ -13,22 +12,60 @@ describe User do
 	it { should respond_to(:email) }
 	it { should respond_to(:password_digest) }
 	it { should respond_to(:password) }
-	it { should respond_to(:password_confirmation) }
 	it { should respond_to(:remember_token) }
+	it { should respond_to(:password_confirmation) }
 	it { should respond_to(:authenticate) }
-	it { should respond_to(:admin) }
+  	it { should respond_to(:admin) }
+  	it { should respond_to(:microposts) }
 
 	it { should be_valid }
-	it { should_not be_admin }
+   	it { should_not be_admin }
 
 	describe "with admin attribute set to 'true'" do
-	  before do
-	    @user.save!
-	    @user.toggle!(:admin)
+	    before do
+	      @user.save!
+	      @user.toggle!(:admin)
+	    end
+
+	    it { should be_admin }
 	  end
 
-	  it { should be_admin }
+
+	describe "micropost associations" do
+
+	    before { @user.save }
+	    let!(:older_micropost) do
+	      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+	    end
+	    let!(:newer_micropost) do
+	      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+	    end
+
+	    it "should have the right microposts in the right order" do
+	      expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+	    end
+
+	    it "should destroy associated microposts" do
+	      microposts = @user.microposts.to_a
+	      @user.destroy
+	      expect(microposts).not_to be_empty
+	      microposts.each do |micropost|
+	        expect(Micropost.where(id: micropost.id)).to be_empty
+	      end
+	    end
+
+	    describe "status" do
+	      let(:unfollowed_post) do
+	        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+	      end
+
+	      its(:feed) { should include(newer_micropost) }
+	      its(:feed) { should include(older_micropost) }
+	      its(:feed) { should_not include(unfollowed_post) }
+	    end
 	end
+
+
 
 	describe "when name is not present" do
 		before { @user.name = " " }
@@ -76,18 +113,8 @@ describe User do
 			it { should_not be_valid }
 		end
 
-		describe "when password is not present" do
-			before do
-				@user = User.new(name: "Example User", email: "user@example.com",
-					password: " ", password_confirmation: " ")
-			end
-			it { should_not be_valid }
-		end
 
-		describe "when password doesn't match confirmation" do
-			before { @user.password_confirmation = "mismatch" }
-			it { should_not be_valid }
-		end
+
 
 		describe "with a password that's too short" do
 			before { @user.password = @user.password_confirmation = "a" * 5 }
@@ -111,7 +138,8 @@ describe User do
 		end
 
 		describe "remember token" do
-			before { @user.save }
-			its(:remember_token) { should_not be_blank }
-		end
+   			before { @user.save }
+    		its(:remember_token) { should_not be_blank }
+  		end
+
 	end
